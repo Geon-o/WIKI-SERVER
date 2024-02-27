88fada50ee80a954ae01243eeaf82eb73ef4903e
geon-o <shjwin96@gmail.com>
2024-02-28 01:08:02 +0900

    [SP-IS-202402281] cross-origin 이슈 해결

---
+++ dev/src/main/java/com/WIKI/dev/init/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.WIKI.dev.init;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+    }
+}
